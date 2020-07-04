9162e212bde6987a1e8b129c906997120fb1359a
thiago <ts.analista@gmail.com>
2020-07-04 20:34:03 -0300

    fix: initial template

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+require __DIR__ . '/fullstackphp/fsphp.php';
+fullStackPHPClassName("ATIVIDADES-01 JULHO 2020");
+//echo '<pre>';
+/*
+ * [ classe e objeto ] http://php.net/manual/pt_BR/language.oop5.basic.php
+ */
+
+fullStackPHPClassSession("duplique se necessários para criar títulos", __LINE__);
+
+
