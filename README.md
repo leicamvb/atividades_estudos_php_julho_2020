db9059cff8a8c8d379977645c03b41e4786e3027
Maciel Rodrigues de Souza <macielrs@grupoprintcor.com.br>
2020-07-07 10:05:55 -0300

    Docs(Inicio) - Exercicio 02 - Enunciado

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # atividades_estudos_php_julho_2020
+
+## Exercicio 02
+##### 2. Com base no exercício 1, popule essa classe com dados e faça imprimir os dados na tela do browser.
+
+* Clone: git clone https://github.com/leicamvb/atividades_estudos_php_julho_2020.git
+* Executado o: npm install
+
+* Criado a branch:
+git checkout -b Exercicio_02
--- index.php
+++ index.php
@@ -6,7 +6,7 @@ fullStackPHPClassName("ATIVIDADES-01 JULHO 2020");
  * [ classe e objeto ] http://php.net/manual/pt_BR/language.oop5.basic.php
  */
 
-fullStackPHPClassSession("duplique se necessários para criar títulos", __LINE__);
-echo '<h1 class="alert alert-success">Tests</h1>';
-
-
+fullStackPHPClassSession("Exercicio 02", __LINE__);
+echo '<h2>2. Com base no exercício 1, popule essa classe com dados e '
+   . 'faça imprimir os dados na tela do browser.</h2>';
+echo '<h1 class="alert alert-success">Resultado</h1>';
