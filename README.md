28e3c93ac6fa3a13af1ff5395b1f2f67125ba05e
Maciel Rodrigues de Souza <macielrs@grupoprintcor.com.br>
2020-07-07 10:11:16 -0300

    Docs(Inicio) - Exercicio 05 - Enunciado

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # atividades_estudos_php_julho_2020
+
+## Exercicio 05
+##### 5. Cria alguma funcionalidade para setar o valor de desconto nos pedidos e imprima na tela.
+
+* Clone: git clone https://github.com/leicamvb/atividades_estudos_php_julho_2020.git
+* Executado o: npm install
+
+* Criado a branch:
+* git checkout -b Exercicio_05
--- index.php
+++ index.php
@@ -6,7 +6,6 @@ fullStackPHPClassName("ATIVIDADES-01 JULHO 2020");
  * [ classe e objeto ] http://php.net/manual/pt_BR/language.oop5.basic.php
  */
 
-fullStackPHPClassSession("duplique se necessários para criar títulos", __LINE__);
-echo '<h1 class="alert alert-success">Tests</h1>';
-
-
+fullStackPHPClassSession("Exercicio 05", __LINE__);
+echo '<h2>5. Cria alguma funcionalidade para setar o valor de desconto nos pedidos e imprima na tela.</h2>';
+echo '<h1 class="alert alert-success">Resultado</h1>';
