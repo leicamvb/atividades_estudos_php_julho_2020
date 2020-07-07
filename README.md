767338c10a1cb13df36df82c3a7ff26c08fce003
Maciel Rodrigues de Souza <macielrs@grupoprintcor.com.br>
2020-07-07 10:00:42 -0300

    Docs(Inicio) - Exercicio 01 - Enunciado

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # atividades_estudos_php_julho_2020
+
+## Exercicio 01
+##### 1. Tente imaginar uma sistema de Restaurante e quais classes um sistemas de Restaurante tem. Pensando o código tem que ter classes, interfaces, propriedades, atributos e métodos.
+
+* Clone: git clone https://github.com/leicamvb/atividades_estudos_php_julho_2020.git
+* Executado o: npm install
+
+* Criado a branch:
+* git checkout -b Exercicio_01
--- index.php
+++ index.php
@@ -6,7 +6,9 @@ fullStackPHPClassName("ATIVIDADES-01 JULHO 2020");
  * [ classe e objeto ] http://php.net/manual/pt_BR/language.oop5.basic.php
  */
 
-fullStackPHPClassSession("duplique se necessários para criar títulos", __LINE__);
-echo '<h1 class="alert alert-success">Tests</h1>';
+fullStackPHPClassSession("Exercicio 01", __LINE__);
+echo '<h2>1. Tente imaginar uma sistema de Restaurante e quais classes um sistemas de Restaurante'
+   . 'tem. Pensando o código tem que ter classes, interfaces, propriedades, atributos e métodos.</h2>';
+echo '<h1 class="alert alert-success">Resultado</h1>';
 
 
